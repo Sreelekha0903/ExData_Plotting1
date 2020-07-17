9cd0dda78509d5daf55f67c1a4607189b4a68ba6
Sreelekha0903 <sreelekhatopalli20@gmail.com>
2020-07-17 10:46:31 -0400

    Plot1.R
    Creating the first plot

---
+++ Plot1.R
@@ -0,0 +1,16 @@
+x <- readline()
+##C:\Users\sreel\Downloads\Data Science Data\ExploratoryDataAnalysis -- Replace working directory here
+setwd(x)
+
+
+##getting the raw data
+rawData <- read.table('household_power_consumption.txt', sep = ";",header = TRUE)
+
+##Formatting the Date column
+rawData$Date <- as.Date(as.character(rawData$Date), "%d/%m/%Y")
+
+##Getting 1st FEb 2007 and 2nd Feb 2007 data
+rawData <- subset(rawData, (Date >= as.Date('2007/02/01') & (Date <= as.Date('2007/02/02'))))
+
+##Plotting the first plot
+hist(as.numeric(rawData$Global_active_power), col = 'red', main = "Global Active Power", xlab = "Global Active Power (kilowatts)", ylab = "Frequency")
