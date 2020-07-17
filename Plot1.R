80ef4771c36a130b6638abc2b48ebec3e430a12d
Sreelekha0903 <sreelekhatopalli20@gmail.com>
2020-07-17 12:39:17 -0400

    Update Plot1.R

--- Plot1.R
+++ Plot1.R
@@ -12,5 +12,10 @@ rawData$Date <- as.Date(as.character(rawData$Date), "%d/%m/%Y")
 ##Getting 1st FEb 2007 and 2nd Feb 2007 data
 rawData <- subset(rawData, (Date >= as.Date('2007/02/01') & (Date <= as.Date('2007/02/02'))))
 
+##Opening graphic device
+dev.new(width = 480,height= 480, unit = "px")
 ##Plotting the first plot
 hist(as.numeric(rawData$Global_active_power), col = 'red', main = "Global Active Power", xlab = "Global Active Power (kilowatts)", ylab = "Frequency")
+
+##closing the connetion
+dev.off()
