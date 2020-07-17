3e0c71e06e14a3399db3e5d700ebf048f021c58b
Sreelekha0903 <sreelekhatopalli20@gmail.com>
2020-07-17 12:40:46 -0400

    Update Plot3.R

--- Plot3.R
+++ Plot3.R
@@ -15,9 +15,14 @@ rawData <- subset(rawData, (Date >= as.Date('2007/02/01') & (Date <= as.Date('20
 ##Creating a new column merging date and time column
 DataForPlot2$DateTimeCol <- paste(DataForPlot2$Date,DataForPlot2$Time, sep = " ")
 
+##Opening graphic device
+dev.new(width = 480,height= 480, unit = "px")
 
 ##Plotting the second plot
 with(DataForPlot2 , plot(as.POSIXct(DateTimeCol), Sub_metering_1, type = "l" , ylab = "Energy sub metering", xlab = ""))
 with(DataForPlot2, lines(as.POSIXct(DateTimeCol), Sub_metering_2, type = "l", col = "red"))
 with(DataForPlot2, lines(as.POSIXct(DateTimeCol), Sub_metering_3, type = "l", col = "blue"))
 legend("topright", legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), col = c("black","red", "blue") , lty = 1, cex = 1)
+
+##closing the connetion
+dev.off()
